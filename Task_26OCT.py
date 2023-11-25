8cd5a0cae7adae09cf29fd51f95174c953dd33b4
Nazhur <125865054+nazhur@users.noreply.github.com>
2023-11-25 21:18:09 +0530

    Add files via upload

---
+++ Task_26OCT.py
@@ -0,0 +1,33 @@
+##TASK-2 26/OCT/2023
+
+## 1. Write a python program to sum all the items in a list?
+
+x=[3,4,5,6,7,7]
+
+total = 0
+
+for number in x:
+    total = total+number
+
+print("Total Sum of numbers in the list:",total)    
+
+
+## 2. Largest number from a list?
+
+x=[13,24,35,16,17,77]
+
+max_num = max(x)
+
+print("The Largest number from the list:", max_num)
+
+
+## 3. Get the smallest number from the list?
+
+
+x=[13,24,35,16,17,77]
+
+min_num = min(x)
+
+print("The Smallest number from the list:", min_num)
+
+
